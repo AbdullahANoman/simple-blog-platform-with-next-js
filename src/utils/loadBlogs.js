7656ea0fc11c356147ad2dbf50157d3dc106a23d
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-09-27 13:08:22 +0600

    change something

--- src/utils/loadBlogs.js
+++ src/utils/loadBlogs.js
@@ -1,6 +1,6 @@
 export const loadBlogs = async () => {
-    const res = await fetch("http://localhost:4000/blogs");
-    const data = await res.json()
-    return data
+    const res = await fetch("https://simple-blog-platform-with-next-js.vercel.app/api/blogs");
+    // const data = await res.json()
+    return res.json()
   };
   
--- src/utils/loadSingleBlog.js
+++ src/utils/loadSingleBlog.js
@@ -1,6 +1,4 @@
 export const loadSingleBlog = async(id) =>{
-    const res = await fetch(`https://simple-blog-platform-with-next-js.vercel.app/api/blogs/${id}`,{
-        cache : "force-cache"
-    })
+    const res = await fetch(`https://simple-blog-platform-with-next-js.vercel.app/api/blogs/${id}`,)
     return res.json()
 }
